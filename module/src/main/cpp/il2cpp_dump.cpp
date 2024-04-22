7b5b701dcb60c090de8f26ae1cda920c914aff0f
ADAMOUMOU <51269456+ADAMOUMOU@users.noreply.github.com>
2024-04-22 21:51:38 +0200

    Update il2cpp_dump.cpp
    Added th array shit

--- module/src/main/cpp/il2cpp_dump.cpp
+++ module/src/main/cpp/il2cpp_dump.cpp
@@ -25,9 +25,35 @@
 
 static uint64_t il2cpp_base = 0;
 
-void init_il2cpp_api(void *handle) {
+// Tableau de correspondance entre les faux noms et les vrais noms
+static std::unordered_map<std::string, std::string> api_name_map = {
+    {"il2cpp_init", "_Znwm74__"},
+    {"il2cpp_method_get_name", "_ZThn99rtSvvPAblA5ivH"},
+    {"il2cpp_field_get_type", "_ZThn685RziUVTpXLw2"},
+    {"il2cpp_field_get_name", "_ZNSt62d6RUNHsJs"},
+    {"il2cpp_field_static_get_value", "_Znwm64rDzQ0jyztCdPn"},
+    {"il2cpp_class_from_type", "_ZNSt35o1CBlzvh8nfBs"},
+    {"il2cpp_class_get_namespace", "_ZNSt47NCEMSwSVQwURTvC"},
+    {"il2cpp_class_get_flags", "_ZNSt694gycfne"},
+    {"il2cpp_class_is_valuetype", "_Znwm52r2xFCk7SluUy"},
+    {"il2cpp_class_is_enum", "_ZNKSt28fP"},
+    {"il2cpp_class_get_parent", "_ZThn7hczNSxmGMnngs"},
+    {"il2cpp_class_get_interfaces", "_ZNKSt65__qB6numrNAomlV9U8"},
+    {"il2cpp_get_corlib", "_ZThn91__C"},
+    {"il2cpp_class_from_name", "_ZSt29wfUfejwdc_"},
+    {"il2cpp_class_get_method_from_name", "_ZNSt81__v2wrLut2xj2TpYLBHCsaX7o"},
+    {"il2cpp_assembly_get_image", "_ZNKSt37__lKDz3nh1"},
+    {"il2cpp_image_get_name", "_ZThn92A"},
+    {"il2cpp_image_get_class_count", "_ZSt64__8Ckl_khqkcwnnWD"},
+    {"il2cpp_image_get_class", "_ZSt40XtwuwTJdCmfi"},
+    {"il2cpp_string_new", "_Znwm56Di"},
+    {"il2cpp_class_from_system_type", "_Znwm96__9w"}
+};
+
+// Macro pour initialiser les pointeurs de fonctions
 #define DO_API(r, n, p) {                      \
-    n = (r (*) p)xdl_sym(handle, #n, nullptr); \
+    std::string real_name = api_name_map[#n];  \
+    n = (r (*) p)xdl_sym(handle, real_name.c_str(), nullptr); \
     if(!n) {                                   \
         LOGW("api not found %s", #n);          \
     }                                          \
