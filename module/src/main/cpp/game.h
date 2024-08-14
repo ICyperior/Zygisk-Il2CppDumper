53fc8c900101dbc2983da6864183687c1faae82d
Cyperior <Digitaldream39@Gmail.com>
2024-08-14 17:49:11 +0300

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.criticalforceentertainment.criticalops"
+#define GamePackageName "com.iov.lordofalgorithms"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
